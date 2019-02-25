38e98ea4b32036fbce3a6a60d00710cc3d81c7dd
Rafael Piloto <31188361+RafaelPiloto10@users.noreply.github.com>
2019-02-24 22:01:30 -0500

    Modified routes. Modified Client file structure. Modified server.js

--- server/server.js
+++ server/server.js
@@ -1,6 +1,29 @@
 const express = require('express');
+const path = require('path');
 const app = express();
+const cors = require('cors');
 const port = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(express.static(path.join(__dirname + "/../client/")));
+
+app.get("/", (req, res, next) => {
+    res.status(200).redirect("/login");
+});
+
+app.get("/login", (req, res, next) => {
+    res.send(200).send("login/");
+});
+
+app.get("/dashboard", (req, res, next) => {
+    res.status(200).send("dashboard/");
+});
+
+app.get("*", (req, res, next) => {
+    res.status(404).json({
+        message: "Page not found"
+    });
+});
 
 app.listen(port, () => {
     console.log("Server is up and running on port " + port);
