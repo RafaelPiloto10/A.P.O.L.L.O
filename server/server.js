7fd6cf08e66713e3ff7396050864b2d8e1985976
Rafael Piloto <31188361+RafaelPiloto10@users.noreply.github.com>
2019-03-04 20:39:18 -0500

    initial commit

--- server/server.js
+++ server/server.js
@@ -2,22 +2,41 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const cors = require('cors');
+const bodyparser = require('body-parser');
+const session = require('express-session');
+
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
+app.use(bodyparser.json());
 app.use(express.static(path.join(__dirname + "/../client/")));
 
 app.get("/", (req, res, next) => {
     res.status(200).redirect("/login");
 });
 
-app.get("/login", (req, res, next) => {
-    res.send(200).send("login/");
-});
+app
+    .get("/login", (req, res, next) => {
+        res.send(200).send("login/");
+    })
+    .post("/login", (req, res, next) => {
+        if (typeof req.body.username != 'undefined' && typeof req.body.password != 'undefined') {
+            res.status(200).json({
+                message: "POST on login was recieved.",
+                error: ""
+            });
+        } else {
+            res.status(200).json({
+                message: "There was an issue with the request",
+                error: "Password or Username are incorrect"
+            })
+        }
+    });
 
 app.get("/dashboard", (req, res, next) => {
     res.status(200).send("dashboard/");
 });
+
 
 app.get("*", (req, res, next) => {
     res.status(404).json({
